2fd745706e4122b173bdf8f0172f8fd870a995fe
priest-2105 <priest2105@gmail.com>
2025-08-02 22:43:32 +0100

    frontend repacked

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    },
-    target: 'es2015',
-    minify: 'esbuild',
-    sourcemap: false
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  }
+  plugins: [react(), tailwindcss()],
 })
